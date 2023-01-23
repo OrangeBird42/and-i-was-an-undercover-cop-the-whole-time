493052411811f0f43b3b520b1d85460127984ff6
Samantha Orange <iamsamanthaorange@gmail.com>
2023-01-23 14:48:56 -0600

    1/23/23 changes

--- PythonApplication3.py
+++ PythonApplication3.py
@@ -1,2 +1,10 @@
 #Input and output Operations
 print("thats pretty gay idk")
+print("man this sucks")
+
+print("     ")
+
+workFile = open('urizons.txt',"r")
+workFileContents = workFile.read()
+print(workFileContents)
+workFile.close()
