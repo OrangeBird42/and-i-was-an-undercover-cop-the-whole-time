acdfeb53e02cf908412dadebbf645d3772727a83
Samantha Orange <iamsamanthaorange@gmail.com>
2023-01-24 14:46:59 -0600

    1/24/23 changes

--- PythonApplication3.py
+++ PythonApplication3.py
@@ -1,10 +1,25 @@
 #Input and output Operations
+
+
+
 print("thats pretty gay idk")
 print("man this sucks")
 
 print("     ")
 
 workFile = open('urizons.txt',"r")
-workFileContents = workFile.read()
-print(workFileContents)
-workFile.close()
+#workFileContents = workFile.read()
+#print(workFileContents)
+print("     ")
+
+#you can only use one, .read() or .readline() not both or else the second in the sequence wont work
+workFileFirstLine = workFile.readline()
+print(workFileFirstLine)
+
+workFileSecondLine = workFile.readline()
+print(workFileSecondLine)
+workFile.close() #make sure to have this *at the end*
+writeFile = open('urizons.txt',"w")
+toLog = input("What do you want to write in the log? ")
+writeFile.toLog
+writeFile.close()
